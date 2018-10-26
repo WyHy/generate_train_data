1f9c85a78f220aac7fe5932e51e5198a10f4434d
Zero <grayautumn15@126.com>
2018-10-26 14:25:31 +0800

    update

--- generate_cell_images_from_xml.py
+++ generate_cell_images_from_xml.py
@@ -15,7 +15,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 import openslide
 
 from constants import CELL_IMAGES_SAVE_PATH, CHECKED_CELL_XML_SAVE_PATH, TIFF_IMAGE_RESOURCE_PATH, TIFF_OPEN_FAIL_RECORDS, \
-    DATA_RESOURCE_ROOT_PATH, SELECTED_CELL_XML_SAVE_PATH
+    DATA_RESOURCE_ROOT_PATH, SELECTED_CELL_XML_SAVE_PATH, METADATA_FILE_PATH
 from tslide.tslide import TSlide
 from utils import FilesScanner, generate_name_path_dict
 
@@ -103,7 +103,9 @@ if __name__ == '__main__':
     executor = ProcessPoolExecutor(max_workers=20)
     tasks = []
 
-    tiff_dict = generate_name_path_dict('', ['.tif', '.kfb'])
+    tif_images_collections_path = os.path.join(METADATA_FILE_PATH, 'TIFF_IMAGES_PATH_DICT.txt')
+    tiff_dict = generate_name_path_dict(tif_path, ['.tif', '.kfb'], tif_images_collections_path)
+    # tiff_dict = generate_name_path_dict('', ['.tif', '.kfb'])
 
     for index, file in enumerate(xmls):
         tasks.append(executor.submit(generate_image_from_xml, file, CELL_IMAGES_SAVE_PATH, tiff_dict))
