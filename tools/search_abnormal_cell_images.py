b6b912d882f7601f0ab40e29877b5799081dff1e
zero <grayautumn15@126.com>
2018-10-27 13:45:17 +0800

    update

--- tools/search_abnormal_cell_images.py
+++ tools/search_abnormal_cell_images.py
@@ -7,26 +7,51 @@ sys.path.append("..")
 
 from utils import FilesScanner
 
-# 1-p0.1718_TC18050036_x34939_y52118_w107_h105_2x.jpg
-# 1-p0.5982_TC18053765_x46070_y20472_w26_h28_.jpg
-pattern = re.compile(r'1-p0.\d{4}_(.*?)_x(\d+)_y(\d+)_w(\d+)_h(\d+)_?(\dx)?.jpg')
 
-src_path = "/home/cnn/Development/DATA/CELL_CLASSIFIED_JOB_20181022/CELLS/TIFFS_CHECKED"
-dst_path = "/home/cnn/Development/DATA/CELL_CLASSIFIED_JOB_20181022/CELLS/ABNORMAL_IMAGE_COLLECTIONS"
+def find_abnormal():
+    # 1-p0.1718_TC18050036_x34939_y52118_w107_h105_2x.jpg
+    # 1-p0.5982_TC18053765_x46070_y20472_w26_h28_.jpg
+    pattern = re.compile(r'1-p0.\d{4}_(.*?)_x(\d+)_y(\d+)_w(\d+)_h(\d+)_?(\dx)?.jpg')
 
-images = FilesScanner(src_path, ['.jpg']).get_files()
+    src_path = "/home/cnn/Development/DATA/CELL_CLASSIFIED_JOB_20181022/CELLS/TIFFS_CHECKED"
+    dst_path = "/home/cnn/Development/DATA/CELL_CLASSIFIED_JOB_20181022/CELLS/ABNORMAL_IMAGE_COLLECTIONS"
 
-for path in images:
-    basename = os.path.basename(path)
-    items = re.findall(pattern, basename)
+    images = FilesScanner(src_path, ['.jpg']).get_files()
 
-    if items:
-        # print(items)
-        pass
-    else:
-        print(basename)
+    for path in images:
+        basename = os.path.basename(path)
+        items = re.findall(pattern, basename)
 
-    big_name, x, y, w, h, _ = items[0]
+        if items:
+            # print(items)
+            pass
+        else:
+            print(basename)
 
-    if int(w) > 500 or int(h) > 500:
-        shutil.copyfile(path, os.path.join(dst_path, "%s_%s_%s_%s_%s.jpg" % (big_name, x, y, w, h)))
+        big_name, x, y, w, h, _ = items[0]
+
+        if int(w) > 500 or int(h) > 500:
+            shutil.copyfile(path, os.path.join(dst_path, "%s_%s_%s_%s_%s.jpg" % (big_name, x, y, w, h)))
+
+
+def get_abnormal_tiff_list():
+    src_path = '/home/cnn/Development/DATA/CELL_CLASSIFIED_JOB_20181022/CELLS/ABNORMAL_IMAGE_COLLECTIONS'
+
+    files = FilesScanner(src_path, ['.jpg']).get_files()
+
+    lst = []
+    for item in files:
+        basename = os.path.basename(item)
+        key = basename.split('_')[0]
+
+        if key in lst:
+            pass
+        else:
+            lst.append(key)
+
+    with open("4x_tiff_lst.txt", 'w') as o:
+        o.write("\n".join(lst))
+
+
+if __name__ == '__main__':
+    get_abnormal_tiff_list()
